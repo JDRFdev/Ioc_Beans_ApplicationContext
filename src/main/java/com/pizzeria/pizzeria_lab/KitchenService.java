1a65ffa7fd33ad9f692809f3f63aef2b65769ee1
Juan Rodriguez <2305juanda@gmail.com>
2026-06-03 22:25:53 -0500

    feat : Add the annotation @ConfigurationProperties

--- src/main/java/com/pizzeria/pizzeria_lab/KitchenService.java
+++ src/main/java/com/pizzeria/pizzeria_lab/KitchenService.java
@@ -1,20 +1,23 @@
 package com.pizzeria.pizzeria_lab;
 
-import jakarta.annotation.PostConstruct;
-import jakarta.annotation.PreDestroy;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.stereotype.Service;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 
+@ConfigurationProperties(prefix="pizzeria")
 public class KitchenService {
 
     private final Chef chef;
-
+    private String name;
+    private String address;
+    private String ovenTemperature;
     public KitchenService( Chef chef) {
         this.chef = chef;
     }
     public void prepareOrder(){
         System.out.println("Here in the Kitchen we prepare delicious plates, Chef says Hello");
         this.chef.SayHello();
+        System.out.println("The Oven's tempeture is: "+this.ovenTemperature);
+        System.out.println("Name: "+this.name);
+        System.out.println("Address: "+this.address);
     }
 
     public void init(){
@@ -23,5 +26,33 @@ public class KitchenService {
     }
     public  void cleanup(){
         System.out.println("We are cleaning up the kitchen");
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public Chef getChef() {
+        return chef;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getOvenTemperature() {
+        return ovenTemperature;
+    }
+
+    public void setOvenTemperature(String ovenTemperature) {
+        this.ovenTemperature = ovenTemperature;
     }
 }
